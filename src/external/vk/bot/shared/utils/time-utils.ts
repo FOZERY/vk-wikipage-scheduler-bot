683062917756ea6b40c280cba653e2d0694cd766
fozery <tagievd31@gmail.com>
2025-02-22 20:41:07 +0300

    fix: parseTimeString regex check (now 111:00 is not valid input)

--- src/external/vk/bot/shared/utils/time-utils.ts
+++ src/external/vk/bot/shared/utils/time-utils.ts
@@ -1,11 +1,11 @@
-import dayjs from 'dayjs';
-import { err, ok } from 'neverthrow';
-import { invalidFormatErr } from '../errors/index.js';
-import { ViewTimeRange } from '../types/common.types.js';
+import dayjs from "dayjs";
+import { err, ok } from "neverthrow";
+import { invalidFormatErr } from "../errors/index.js";
+import { ViewTimeRange } from "../types/common.types.js";
 
 export function parseTimeString(input: string) {
-	const timeRangeRegex = /(\d{2}:\d{2})\s*-\s*(\d{2}:\d{2})/;
-	const singleTimeRegex = /(\d{2}:\d{2})/;
+	const timeRangeRegex = /^(\d{2}:\d{2})\s*-\s*(\d{2}:\d{2})$/;
+	const singleTimeRegex = /^(\d{2}:\d{2})$/;
 
 	let startTimeString, endTimeString: string | null;
 
@@ -13,32 +13,32 @@ export function parseTimeString(input: string) {
 		// Если введен промежуток времени
 		const [, rawStartTime, rawEndTime] = input.match(timeRangeRegex)!;
 
-		const startTime = dayjs(rawStartTime, 'HH:mm', true);
-		const endTime = dayjs(rawEndTime, 'HH:mm', true);
+		const startTime = dayjs(rawStartTime, "HH:mm", true);
+		const endTime = dayjs(rawEndTime, "HH:mm", true);
 
 		if (!endTime.isValid() || !startTime.isValid()) {
-			return err(invalidFormatErr(input, 'HH:mm - HH:mm'));
+			return err(invalidFormatErr(input, "HH:mm - HH:mm"));
 		}
 
 		if (startTime.diff(endTime) > 0) {
-			return err(invalidFormatErr(input, 'HH:mm - HH:mm'));
+			return err(invalidFormatErr(input, "HH:mm - HH:mm"));
 		}
 
-		startTimeString = startTime.format('HH:mm:ss');
-		endTimeString = endTime.format('HH:mm:ss');
+		startTimeString = startTime.format("HH:mm:ss");
+		endTimeString = endTime.format("HH:mm:ss");
 	} else if (singleTimeRegex.test(input)) {
 		// Если введено только время начала
 		const [, rawStartTime] = input.match(singleTimeRegex)!;
 
-		const startTime = dayjs(rawStartTime, 'HH:mm', true);
+		const startTime = dayjs(rawStartTime, "HH:mm", true);
 		if (!startTime.isValid()) {
-			return err(invalidFormatErr(rawStartTime, 'HH:mm'));
+			return err(invalidFormatErr(rawStartTime, "HH:mm"));
 		}
 
-		startTimeString = startTime.format('HH:mm:ss');
+		startTimeString = startTime.format("HH:mm:ss");
 		endTimeString = null;
 	} else {
-		return err(invalidFormatErr(input, 'HH:mm - HH:mm'));
+		return err(invalidFormatErr(input, "HH:mm - HH:mm"));
 	}
 
 	return ok({ startTimeString, endTimeString });
@@ -46,15 +46,15 @@ export function parseTimeString(input: string) {
 
 export function timeRangeToStringOutput(timeRange: ViewTimeRange): string {
 	if (!timeRange) {
-		return 'Не указано';
+		return "Не указано";
 	}
 
 	if (!timeRange.endTime) {
-		return `${dayjs(timeRange.startTime, 'HH:mm:ss').format('HH:mm')}`;
+		return `${dayjs(timeRange.startTime, "HH:mm:ss").format("HH:mm")}`;
 	}
 
-	return `${dayjs(timeRange.startTime, 'HH:mm:ss').format('HH:mm')} - ${dayjs(
+	return `${dayjs(timeRange.startTime, "HH:mm:ss").format("HH:mm")} - ${dayjs(
 		timeRange.endTime,
-		'HH:mm:ss'
-	).format('HH:mm')}`;
+		"HH:mm:ss"
+	).format("HH:mm")}`;
 }
