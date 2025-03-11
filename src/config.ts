481993c8b3d7e10337ec277d83edf8e9f125b946
fozery <tagievd31@gmail.com>
2025-03-11 15:46:36 +0300

    feat: add supabase to package.json, change config.toml, change config.ts/env.example

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,6 @@ const envSchema = z.object({
 	PAGE_ID: z.coerce.number(),
 	PAGE_URL: z.string().url(),
 	DATABASE_URL: z.string().url(),
-	SUPABASE_URL: z.string().url(),
-	SUPABASE_API_KEY: z.string(),
 	LOGTAIL_TOKEN: z.string(),
 	LOGTAIL_URL: z.string().url(),
 	LOG_TO_LOGTAIL: z.enum(["true", "false"]).transform((val) => val === "true"),
